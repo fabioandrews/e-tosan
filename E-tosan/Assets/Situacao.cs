08ae762f67be2da8835e0a0ca47ce072b3d4b6cc
fábio andrews rocha marques <fabio_andrews@hotmail.com>
2016-07-01 11:26:57 -0300

    Commit inicial. Apenas o esboço do let's jam(ainda nem fiz suporte a multilinguagem)

---
+++ E-tosan/Assets/Situacao.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Situacao 
+{
+	private ArrayList temposVerbaisEstudados;
+	private string verbo;
+	private string lugar;
+	private string depoimentoMelody;
+	private string depoimentoRegras; //depoimento da mocinha que diz as regras
+	private ArrayList duasRespostasDoEtosan; // e-tosan soh pode dizer duas respostas
+	private bool respostaAgradaMelody;
+
+	public Situacao(ArrayList temposVerbaisEstudados, string verbo, string lugar, string depoimentoMelody, string depoimentoRegras, ArrayList duasRespostasDoEtosan, bool respostaAgradaMelody)
+	{
+		this.temposVerbaisEstudados = temposVerbaisEstudados;
+		this.verbo = verbo;
+		this.lugar = lugar;
+		this.depoimentoMelody = depoimentoMelody;
+		this.depoimentoRegras = depoimentoRegras;
+		this.duasRespostasDoEtosan = duasRespostasDoEtosan;
+		this.respostaAgradaMelody = respostaAgradaMelody;
+	}
+}
---
+++ E-tosan/Assets/codigoBotoesLetsJam/BotaoPause.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotaoPause : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void OnMouseDown()
+    {
+        GameObject instanciaPopupPauseLetsJam = GameObject.Find("CanvasPopupPauseLetsJam");
+        PopupPauseLetsJam instanciaPopupPauseLetsJamComTipoReal = instanciaPopupPauseLetsJam.GetComponent<PopupPauseLetsJam>(); ;
+        instanciaPopupPauseLetsJamComTipoReal.voltarAPosicaoInicial();
+    }
+}
